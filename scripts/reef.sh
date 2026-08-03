684ae6ee17f678cd2443088e006e0d5af88f51e1
sinclair-29 <732277968@qq.com>
2026-08-03 11:18:26 +0800

    chore: create config/reef_smoke.yaml config/benchmark_reef_smoke.yaml

--- scripts/reef.sh
+++ scripts/reef.sh
@@ -1,5 +1,5 @@
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 \
-CUDA_VISIBLE_DEVICES=2,3 python main.py \
-    --benchmark_config 'config/benchmark_config.yaml' \
-    --fingerprint_config 'config/reef.yaml' \
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --benchmark_config 'config/benchmark_reef_smoke.yaml' \
+    --fingerprint_config 'config/reef_smoke.yaml' \
     --log_path 'logs/'
--- scripts/trap.sh
+++ scripts/trap.sh
@@ -1,5 +1,5 @@
 HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 \
-CUDA_VISIBLE_DEVICES=4,5 python main.py \
-    --benchmark_config 'config/benchmark_config.yaml' \
-    --fingerprint_config 'config/trap.yaml' \
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --benchmark_config 'config/benchmark_trap_smoke.yaml' \
+    --fingerprint_config 'config/trap_smoke.yaml' \
     --log_path 'logs/'
